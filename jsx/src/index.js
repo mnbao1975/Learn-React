4430f44595486a5426f08109eed960e2b92724fd
baonguyen <bao.nm@ringier.com.vn>
2019-03-04 16:57:02 +0700

    continue

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,14 +1,22 @@
 // Import the React and ReactDOM libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+
+function getButtonText() {
+  return 'Click on me';
+}
 
 // Create a react component
 const App = () => {
+  const buttonText = 'Click me!'
+
   return (
   <div>
-    <label class='label' for='name'>Name</label>
-    <input id='name' type='text'/>
-    <button style='background-color:blue; color:white;'>Submit</button>
+    <label className="label" for="name">Name</label>
+    <input id="name" type="text"/>
+    <button style={{ backgroundColor: 'blue', color: 'white' }}>
+      {getButtonText()}
+    </button>
   </div>
   );
 };
@@ -16,5 +24,5 @@ const App = () => {
 // Take the react component and show it on the screen
 ReactDOM.render(
   <App />,
-  document.querySelector('#root')
+  document.querySelector("#root")
 );
