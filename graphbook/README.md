71f3ce922913c2888bac9b4b0133c4fac6f4e3ec
Bao Nguyen Minh <baonguyen@Baos-MacBook-Pro.local>
2019-12-04 14:25:16 +0700

    updated

---
+++ graphbook/README.md
@@ -0,0 +1,3 @@
+Source code
+
+https://github.com/PacktPublishing/Hands-on-Full-Stack-Web-Development-with-GraphQL-and-React
---
+++ graphbook/src/server/index.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-console */
+import express from 'express';
+import path from 'path';
+import helmet from 'helmet';
+import cors from 'cors';
+import compress from 'compression';
+
+const app = express();
+const root = path.join(__dirname, '../../');
+
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", "'unsafe-inline'"],
+    styleSrc: ["'self'", "'unsafe-inline'"],
+    imgSrc: ["'self'", 'data:', '*.amazonaws.com'],
+  },
+}));
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
+app.use(compress());
+app.use(cors());
+
+app.use('/', express.static(path.join(root, 'dist/client')));
+app.use('/uploads', express.static(path.join(root, 'uploads')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(root, 'dist/client/index.html'));
+});
+
+app.listen(8000, () => console.log('Listening on port 8000'));
